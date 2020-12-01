8b2b97c0d4db3f32737ed816c9158667d95d1194
Collin Newman <collin.newman37@gmail.com>
2020-12-01 11:51:32 -0500

    Implemented rockPaperScissors

--- rockPaperScissors/rockPaperScissors.js
+++ rockPaperScissors/rockPaperScissors.js
@@ -18,8 +18,26 @@
 *
 */
 
-var rockPaperScissors = function (
-) {
-  // TODO: your solution here
+var rockPaperScissors = function () {
+  let results = [];
+  obj = { 0: 'R', 1: 'P', 2: 'S' };
+  array = [0, 0, 0];
+  for (let j = 0; j < 3; j++) {
+    for (let i = 0; i < 3; i++) {
+      while (array[2] < 3) {
+        results.push([obj[array[0]], obj[array[1]], obj[array[2]]]);
+        array[2]++;
+      }
+      array[2] = 0;
+      array[1]++;
+    }
+    array[0] = 0;
+    array[1] = 0;
+    array[2] = 0;
+    let oldObj0 = obj[0];
+    obj[0] = obj[1];
+    obj[1] = obj[2];
+    obj[2] = oldObj0;
+  }
+  return results;
 };
-
