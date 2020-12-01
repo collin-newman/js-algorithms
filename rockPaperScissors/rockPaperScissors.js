3d0ad064ebda1370198f37b48a3d40d1117ca255
Collin Newman <collin.newman37@gmail.com>
2020-12-01 11:54:42 -0500

    Changed rockPaperSissors to return an array of strings

--- rockPaperScissors/rockPaperScissors.js
+++ rockPaperScissors/rockPaperScissors.js
@@ -25,7 +25,7 @@ var rockPaperScissors = function () {
   for (let j = 0; j < 3; j++) {
     for (let i = 0; i < 3; i++) {
       while (array[2] < 3) {
-        results.push([obj[array[0]], obj[array[1]], obj[array[2]]]);
+        results.push([obj[array[0]] + obj[array[1]] + obj[array[2]]]);
         array[2]++;
       }
       array[2] = 0;
