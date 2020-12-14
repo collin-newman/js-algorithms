74db46bf6766b6298d11a77b0b52f7c5db4d1a46
Collin Newman <collin.newman37@gmail.com>
2020-12-14 11:32:25 -0500

    Implemented nonreapeatedCharacter(). Time Complexity: Linear if there are few nonrepeated character, or exponential if given a string of mostly nonreapeated characters.

--- nonrepeatedCharacter/nonrepeatedCharacter.js
+++ nonrepeatedCharacter/nonrepeatedCharacter.js
@@ -5,7 +5,21 @@
  *   firstNonRepeatedCharacter('ABA'); // => 'B'
  *   firstNonRepeatedCharacter('AACBDB'); // => 'C'
  */
-
 var firstNonRepeatedCharacter = function(string) {
-  // TODO: your solution here
+  let tracker = {};
+
+  for (let index = 0; index < string.length; index++) {
+    if (tracker[string[index]] === undefined) {
+      tracker[string[index]] = {count: 1, index: index, character: string[index]};
+    } else {
+      tracker[string[index]].count++;
+    }
+  }
+  let uniques = Object.values(tracker).filter((char) => (
+    char.count === 1
+  ));
+  let sortedUniques = uniques.sort((a, b) => (
+    a.index < b.index
+  ))
+  return sortedUniques[0].character;
 };
