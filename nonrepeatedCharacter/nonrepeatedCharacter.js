bf60ddaf829fa536e05c5ba3714777c1618af4fb
Collin Newman <collin.newman37@gmail.com>
2020-12-14 11:48:37 -0500

    Changed the main for loop to only only add the item to the tracker if it has already not been check previously. Last implementation only worked for even length inputs.

--- nonrepeatedCharacter/nonrepeatedCharacter.js
+++ nonrepeatedCharacter/nonrepeatedCharacter.js
@@ -6,16 +6,23 @@
  *   firstNonRepeatedCharacter('AACBDB'); // => 'C'
  */
 var firstNonRepeatedCharacter = function(string) {
-  let tracker = {};
+  let tracker = {
+      alreadyChecked: []
+  };
   if (string.length === 0) { return null; }
 
   for (let index = 0; index < string.length; index++) {
-    if (tracker[string[index]] === undefined) {
+    if (tracker[string[index]] === undefined && !tracker.alreadyChecked.includes(string[index])) {
       tracker[string[index]] = {count: 1, index: index, character: string[index]};
     } else {
+      if (!tracker.alreadyChecked.includes(string[index])) {
+        tracker.alreadyChecked.push(string[index]);
+      }
       delete tracker[string[index]];
     }
   }
+  delete tracker.alreadyChecked;
+
   let sortedUniques = Object.values(tracker).sort((a, b) => (
     a.index < b.index
   ))
