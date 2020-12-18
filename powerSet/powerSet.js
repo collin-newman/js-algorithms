c477608cd0f935d3e5c31f7ca3adb5f37910074f
Collin Newman <collin.newman37@gmail.com>
2020-12-18 11:50:38 -0500

    Implemented powerSet but it produces duplicates

--- powerSet/powerSet.js
+++ powerSet/powerSet.js
@@ -7,15 +7,45 @@
  * powerSet("abc")
  * -> [ '' , 'a', 'b', 'c', 'ab', 'ac', 'bc', 'abc' ]
  *
- * Note: 
+ * Note:
  *  1. All characters in a subset should be sorted.
- *  2. Sets of the same characters are considered duplicates regardless of order and count only once, e.g. 'ab' and 'ba' are the same. 
- * 
+ *  2. Sets of the same characters are considered duplicates regardless of order and count only once, e.g. 'ab' and 'ba' are the same.
+ *
  * Example 2 :
- * 
+ *
  * powerSet("jump")
  * -> ["", "j", "ju", "jm", "jp", "jmu", "jmp", "jpu", "jmpu", "u", "m", "p", "mu", "mp", "pu", "mpu"]
  */
 
 var powerSet = function(str) {
+  let results = [str, ''];
+
+  let findSet = function(string) {
+
+    for (let i = 0; i < string.length; i++) {
+      let head = string.slice(i, i + 1);
+      let tailArr = string.split('');
+      tailArr.splice(i, 1);
+
+      let tail = tailArr.join('');
+      let set = head;
+      console.log('head', head);
+      console.log('tail', tail);
+      for (let j = 0; j < tail.length - 1; j++) {
+        set += tail[j];
+      }
+      if (!results.includes(set)) {
+        results.push(set);
+      }
+      if (head.length !== set.length) {
+        findSet(set);
+      }
+    }
+  }
+
+  findSet(str);
+
+  return results;
 };
+
+powerSet('jump');
