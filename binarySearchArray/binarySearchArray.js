18605b0840cc3cdcd764f11851007b6c9107d774
Collin Newman <collin.newman37@gmail.com>
2021-01-05 11:56:06 -0500

    binarySearchArray: fails to return the proper index of the target

--- binarySearchArray/binarySearchArray.js
+++ binarySearchArray/binarySearchArray.js
@@ -11,5 +11,31 @@
  */
 
 var binarySearch = function (array, target) {
+  let index = null;
+  let start = 0;
+  let end = array.length - 1;
+  let mid = Math.floor(arr.length / 2);
+  if (array[mid] === target) { return mid; };
+
+  let recursor = (arr) => {
+    mid = Math.floor(arr.length / 2);
+    if (target === arr[mid]) {
+      if (mid = 0) {
+        index = start;
+      } else if (mid = array.length - 1) {
+        index = end;
+      } else {
+        index = end - start;
+      }
+    } else if (target > arr[mid]) {
+      start = mid + 1;
+      recursor(arr.slice(mid + 1));
+    } else if (target < arr[mid]) {
+      end = mid - 1;
+      recursor(arr.slice(0, mid));
+    }
+  }
+  recursor(array, target);
+  return index;
 };
 
