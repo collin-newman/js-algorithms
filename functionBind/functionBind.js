cbbf43826246b541e3b56a7c6674dfee0c03be82
Collin Newman <collin.newman37@gmail.com>
2021-01-14 11:39:39 -0500

    Implemented bind and Function.prototype.bind

--- functionBind/functionBind.js
+++ functionBind/functionBind.js
@@ -3,57 +3,57 @@
  *
  * example 1:
  *
- * var alice = {
- *   name: 'alice',
- *   shout: function(){
- *     alert(this.name);
- *   }
- * }
- * var boundShout = bind(alice.shout, alice);
- * boundShout(); // alerts 'alice'
- * boundShout = bind(alice.shout, {name: 'bob'});
- * boundShout(); // alerts 'bob'
+var alice = {
+  name: 'alice',
+  shout: function(){
+    alert(this.name);
+  }
+}
+var boundShout = bind(alice.shout, alice);
+boundShout(); // alerts 'alice'
+boundShout = bind(alice.shout, {name: 'bob'});
+boundShout(); // alerts 'bob'
  *
  * example 2:
  *
- * var func = function(a, b){ return a + b };
- * var boundFunc = bind(func, null, 'foo');
- * var result = boundFunc('bar');
- * result === 'foobar'; // true
+var func = function(a, b){ return a + b };
+var boundFunc = bind(func, null, 'foo');
+var result = boundFunc('bar');
+result === 'foobar'; // true
  *
 */
 
-var bind = function(
-) {
+var bind = function(fn, obj, ...args) {
   // TODO: Your code here
+  return (...otherArgs) => fn.apply(obj, args.concat(otherArgs))
 };
+
 
 /*
  * Function.prototype.bind:
  *
  * example 1:
  *
- * var alice = {
- *   name: 'alice',
- *   shout: function(){
- *     alert(this.name);
- *   }
- * }
- * var boundShout = alice.shout.bind(alice);
- * boundShout(); // alerts 'alice'
- * boundShout = alice.shout.bind({name: 'bob'});
- * boundShout(); // alerts 'bob'
- *
- * example 2:
- *
- * var func = function(a, b){ return a + b };
- * var boundFunc = func.bind(null, 'foo');
- * var result = boundFunc('bar');
- * result === 'foobar'; // true
- *
-*/
+var alice = {
+  name: 'alice',
+  shout: function(){
+    alert(this.name);
+  }
+}
+var boundShout = alice.shout.bind(alice);
+boundShout(); // alerts 'alice'
+boundShout = alice.shout.bind({name: 'bob'});
+boundShout(); // alerts 'bob'
 
-Function.prototype.bind = function(
-) {
+ example 2:
+
+var func = function(a, b){ return a + b };
+var boundFunc = func.bind(null, 'foo');
+var result = boundFunc('bar');
+result === 'foobar'; // true
+ */
+
+Function.prototype.bind = function(obj, ...args) {
   // TODO: Your code here
+  return (...otherArgs) => this.apply(obj, args.concat(otherArgs))
 };
