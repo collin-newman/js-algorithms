e3f84531c8e6e1ac2dee2184c629e6863a319304
Collin Newman <collin.newman37@gmail.com>
2021-01-22 11:25:12 -0500

    Implemented linkedListIntersection

--- linkedListIntersection/linkedListIntersection.js
+++ linkedListIntersection/linkedListIntersection.js
@@ -2,29 +2,29 @@
 *
 * Linked List Intersection
 *
-* Write a function linkedListIntersection that returns the node 
-* at which the intersection of two linked lists begins, 
+* Write a function linkedListIntersection that returns the node
+* at which the intersection of two linked lists begins,
 * or null if there is no such intersection.
-* 
+*
 * Example:
-* 
-* Given the following two linked lists list1 and list2, 
-* linkedListIntersection(list1,list2) should return D 
+*
+* Given the following two linked lists list1 and list2,
+* linkedListIntersection(list1,list2) should return D
 * as the node of intersection.
-* 
+*
 *    A → B → C
 *             ↘
 *               D → E → F
 *             ↗
 *        X → Y
-* 
-* Given the following two linked lists list1 and list2, 
-* linkedListIntersection(list1,list2) should return NULL 
+*
+* Given the following two linked lists list1 and list2,
+* linkedListIntersection(list1,list2) should return NULL
 * as there is no point of intersection.
-* 
+*
 *    A → B → C → D
 *    X → Y → Z
-* 
+*
 */
 
 // Helper function (do not edit)
@@ -39,5 +39,20 @@ function Node (val) {
 
 function linkedListIntersection(list1, list2) {
   // TODO: Implement this function!
+  const reverseLinkedList = (list) => {
+    const listItems = [];
+    while (list.next !== null) {
+      listItems.push(list.value);
+      list = list.next;
+    }
+    return listItems.reverse();
+  }
+  const array1 = reverseLinkedList(list1);
+  const array2 = reverseLinkedList(list2);
+  for (let i = 0; i < array1.length; i++) {
+    if (array1[i] !== array2[i]) {
+      return array1[i - 1];
+    }
+  }
+  return null;
 }
-
