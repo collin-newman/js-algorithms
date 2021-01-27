5786afa8d309ea6b153dfbf0dbe0bfb38ef733d4
Collin Newman <collin.newman37@gmail.com>
2021-01-27 11:32:34 -0500

    Completed integerReverse. I had to google this one, I was completely lost on how to make this work for an integer of any size.

--- integerReverse/integerReverse.js
+++ integerReverse/integerReverse.js
@@ -1,14 +1,25 @@
-/* 
-* 
+/*
+*
 * Integer Reverse
-* 
-* Given a positive integer, return its digits reversed. 
-* 
-* - DO NOT CONVERT THE INPUT INTO A STRING OR ARRAY. 
+*
+* Given a positive integer, return its digits reversed.
+*
+* - DO NOT CONVERT THE INPUT INTO A STRING OR ARRAY.
 * - Only use integers and math in your solution.
 *
 */
 
 function reverseInteger(number){
   // TODO: Implement this function!
+  let lastDigit;
+  let reverse = 0;
+  let number = number;
+
+  while (number > 0) {
+    lastDigit = number % 10;
+    reverse = (reverse * 10) + lastDigit;
+    number = number - lastDigit;
+    number = number / 10;
+  }
+  return reverse;
 }
--- largestProductOfThree/largestProductOfThree.js
+++ largestProductOfThree/largestProductOfThree.js
@@ -10,11 +10,9 @@
 
 var largestProductOfThree = function(array) {
   let sortedArr = array.sort((a, b) => (b - a));
-  for (let i = 0; i < 2; i++) {
-    if (Math.abs(sortedArr[sortedArr.length -1]) > sortedArr[2]) {
-      let deleted = sortedArr.splice(sortedArr.length - 1);
-      sortedArr.unshift(Math.abs(deleted[0]));
-    }
+  if (Math.abs(sortedArr[sortedArr.length -1]) > sortedArr[2] && Math.abs(sortedArr[sortedArr.length - 2]) > sortedArr[2]) {
+    let deleted = sortedArr.splice(sortedArr.length - 1, 2);
+    sortedArr.unshift(Math.abs(sortedArr.concat(deleted)));
   }
   sortedArr = sortedArr.sort((a, b) => (b - a));
   return sortedArr.reduce((acc, cur, index) => {
