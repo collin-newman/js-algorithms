cc3c7757c0aafdbd3c60d369814aaf7516cbe6a4
Collin Newman <collin.newman37@gmail.com>
2021-02-08 12:24:50 -0500

    Implemented spiralTraversal. no edge cases

--- spiralTraversal/spiralTraversal.js
+++ spiralTraversal/spiralTraversal.js
@@ -5,16 +5,62 @@
  *
  * example:
 
-    spiralTraversal([
-      [1,2,3],
-      [4,5,6],
-      [7,8,9]
-    ]);
+spiralTraversal([
+  [1,2,3],
+  [4,5,6],
+  [7,8,9]
+]);
 
     returns [1, 2, 3, 6, 9, 8, 7, 4, 5]
  */
 
-var spiralTraversal = function(matrix) {
 
-  // TODO: Implement me!
+var spiralTraversal = function(matrix) {
+  const upperRow = (twoDimArray, acc) => {
+    const result = twoDimArray.splice(0, 1);
+    result[0].forEach((item) => acc.push(item));
+
+    if (twoDimArray.length === 0) {
+      finalResult = acc;
+      return;
+    }
+    rightColumn(twoDimArray, acc);
+  }
+
+  const rightColumn = (twoDimArray, acc) => {
+    if (twoDimArray.length === 1) {
+      finalResult = acc;
+      return;
+    }
+    const result = [];
+    twoDimArray.forEach((item) => result.push(item.splice(item.length - 1, 1)));
+    result.forEach((item) => acc.push(item[0]));
+    lowerRow(twoDimArray, acc);
+  }
+
+  const lowerRow = (twoDimArray, acc) => {
+    if (twoDimArray.length === 1) {
+      finalResult = acc;
+      return;
+    }
+    const result = twoDimArray.splice(twoDimArray.length - 1, 1);
+    result[0].forEach((item) => acc.push(item));
+    leftColumn(twoDimArray, acc);
+  }
+
+  const leftColumn = (twoDimArray, acc) => {
+    if (twoDimArray[0].length === 1) {
+      acc.push(twoDimArray[0][0]);
+      finalResult = acc;
+      return;
+    }
+    const result = [];
+    twoDimArray.reverse().forEach((item) => result.push(item.splice(0, 1)));
+    result[0].forEach((item) => acc.push(item));
+    upperRow(twoDimArray, acc);
+  }
+
+  const finalResult = null;
+  upperRow(matrix, []);
+  return finalResult;
 };
