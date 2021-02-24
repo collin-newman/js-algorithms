033dad47109e18e187dc5b233441175e52fca6f1
Collin Newman <collin.newman37@gmail.com>
2021-02-24 11:51:26 -0500

    Implemented linkedList in pseoudoclassical instantiantion pattern

--- linkedList/linkedList.js
+++ linkedList/linkedList.js
@@ -20,22 +20,40 @@
 
 
 var LinkedList = function() {
-  //fill me in!
+  this.storage = [];
+  this.head = {};
+  this.tail = {};
 };
 
 //write methods here!
 
-LinkedList.prototype.addToTail = function(
-) {
+LinkedList.prototype.addToTail = function(value) {
+  this.storage.push(LinkedList.prototype.makeNode(value));
+  let previousElement = this.storage.length - 2 > 0 ? this.storage.length - 2 : 0;
+  let thisElement = this.storage.length - 1 > 1 ? this.storage[this.storage.length - 1] : null;
+  this.storage[previousElement][1] = thisElement;
+  this.head.value = this.storage[0][0];
+  this.tail.value = this.storage[this.storage.length - 1][0];
 };
 
 LinkedList.prototype.removeHead = function() {
+  let head = this.storage.shift();
+  this.head.value = this.storage.length > 0 ? this.storage[0][0] : null;
+  let value = head !== undefined ? head[0] : null;
+  return value;
 };
 
-LinkedList.prototype.contains = function(
-) {
+LinkedList.prototype.contains = function(node) {
+  let contains = false;
+  for (let i = 0; i < this.storage.length; i++) {
+    if (this.storage[i] === node) {
+      contains = true;
+      break;
+    }
+  }
+  return contains;
 };
 
-LinkedList.prototype.makeNode = function(
-) {
-};
+LinkedList.prototype.makeNode = function(value) {
+  return [value, null];
+}
